4685f83646a90df067b107e107a7d2f38094a200
Sameer Prasad Koppolu <sameerkoppolu@gmail.com>
2024-07-18 19:48:22 +0000

    Updating hello.py and test_hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 def add_nos(a, b, c):
     return a + b + c
+
+def subtract_nos(a,b,c):
+    return a - b - c
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,8 @@
-from hello import add_nos
+from hello import add_nos, subtract_nos
 
 
 def test_add_nos():
     assert add_nos(1, 2, 3) == 6
+
+def test_subtract_nos():
+    assert subtract_nos(1,2,3) == -4
